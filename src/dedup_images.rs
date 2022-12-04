f5d036efa6723d00798cc2d15153507bc7e68c69
Alex Nichol <unixpickle@gmail.com>
2022-12-04 12:22:15 -0500

    perceptual hash

--- src/dedup_images.rs
+++ src/dedup_images.rs
@@ -38,3 +38,62 @@ pub struct Args {
 pub async fn main(args: Args) -> anyhow::Result<()> {
     Ok(())
 }
+
+fn hash_and_downsample(args: &Args, path: &Path) -> anyhow::Result<String> {
+    let mut img = image::io::Reader::open(path)?
+        .with_guessed_format()?
+        .decode()?;
+    let hash = hash_image(args.hash_resolution, &img)?;
+
+    let in_width = img.width();
+    let in_height = img.height();
+    let scale = (args.out_resolution as f64) / (in_width.min(in_height) as f64);
+    if scale < 1.0 {
+        img = img.resize(
+            ((in_width as f64) * scale) as u32,
+            ((in_height as f64) * scale) as u32,
+            FilterType::Lanczos3,
+        );
+    }
+    let out_path: PathBuf = [&args.output_dir, &hash].iter().collect();
+    let tmp_out_path: PathBuf = [&args.output_dir, &format!("tmp_{}", path_basename(path))]
+        .iter()
+        .collect();
+    img.save_with_format(&tmp_out_path, ImageFormat::Jpeg)?;
+    rename(tmp_out_path, out_path)?;
+    Ok(hash)
+}
+
+fn hash_image(resolution: u32, img: &DynamicImage) -> anyhow::Result<String> {
+    let orig_width = img.width();
+    let orig_height = img.height();
+    let small_img = img
+        .resize_exact(resolution, resolution, FilterType::Lanczos3)
+        .into_rgb8();
+
+    let mut bytes = Vec::new();
+    for px in small_img.as_bytes() {
+        // Quantize each color to allow some wiggle room.
+        bytes.push(px >> 4);
+    }
+
+    // Bin the aspect ratio to make sure we don't match very
+    // differently sized images.
+    let log_aspect_ratio = ((((orig_width as f64) / (orig_height as f64)).log2())
+        .clamp(-4.0, 4.0)
+        .round()
+        + 4.0) as u8;
+    bytes.push(log_aspect_ratio);
+
+    let mut hasher = sha2::Sha256::new();
+    hasher.update(&bytes);
+    let mut res = String::with_capacity(64);
+    for ch in hasher.finalize() {
+        write!(&mut res, "{:02x}", ch).unwrap();
+    }
+    Ok(res)
+}
+
+fn path_basename(p: &Path) -> String {
+    p.file_name().unwrap().to_string_lossy().into_owned()
+}
