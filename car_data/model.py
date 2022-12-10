3d70d065206ebf23c38b3bb72be3a808f42141be
Alex Nichol <unixpickle@gmail.com>
2022-12-09 22:49:45 -0500

    vision model

---
+++ car_data/model.py
@@ -0,0 +1,22 @@
+from typing import Dict, Optional
+
+import clip
+import torch
+import torch.nn as nn
+
+from .losses import NUM_PRICE_BINS
+
+
+class CLIPModel(nn.Module):
+    def __init__(self, device: torch.device, download_root: Optional[str] = None):
+        super().__init__()
+        self.device = device
+        self.clip, _ = clip.load("ViT-B/16", device=device, download_root=download_root)
+        self.clip.float()
+        self.price_layer = nn.Linear(512, NUM_PRICE_BINS, device=device)
+
+    def forward(self, x: torch.Tensor) -> Dict[str, torch.Tensor]:
+        feats = self.clip.encode_image(x)
+        return dict(
+            price_bin=self.price_layer(feats),
+        )
