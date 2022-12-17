66de6719c5d31950d9215d416ad91de2b1421bf0
Alex Nichol <unixpickle@gmail.com>
2022-12-17 12:06:07 -0500

    compute features of dataset

---
+++ car_data/scripts/clip_features.py
@@ -0,0 +1,63 @@
+"""
+Compute three-crop CLIP features for all the images in a dataset to apply to
+filtering.
+"""
+
+import argparse
+import os
+from collections import defaultdict
+from typing import Iterator, List, Tuple
+
+import clip
+import numpy as np
+import torch
+from car_data.lin_features import compute_pooled_features
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--shard_digits", type=int, default=4)
+    parser.add_argument("--download_root", type=str, default=None)
+    parser.add_argument("--batch_size", type=int, default=64)
+    parser.add_argument("image_dir", type=str)
+    parser.add_argument("output_dir", type=str)
+    args = parser.parse_args()
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model, preprocess = clip.load(
+        "ViT-B/16", device=device, download_root=args.download_root
+    )
+
+    print("reading paths...")
+    listing = sorted(os.listdir(args.image_dir))
+    print("iterating...")
+    for shard_id, filenames in group_by_prefix(listing, args.shard_digits):
+        out_path = os.path.join(args.output_dir, f"{shard_id}.npz")
+        if os.path.exists(out_path):
+            continue
+        print(f"working on {out_path}...")
+        features = compute_pooled_features(
+            device,
+            model,
+            preprocess,
+            [os.path.join(args.image_dir, x) for x in filenames],
+            batch_size=args.batch_size,
+        )
+        np.savez(out_path + ".tmp.npz", features=features, filenames=filenames)
+        os.rename(out_path + ".tmp.npz", out_path)
+
+
+def group_by_prefix(
+    listing: List[str], prefix_len: int
+) -> Iterator[Tuple[str, List[str]]]:
+    groups = defaultdict(list)
+    for item in listing:
+        if len(item) < prefix_len or item.startswith("."):
+            continue
+        groups[item[:prefix_len]].append(item)
+    for k in sorted(groups.keys()):
+        yield k, groups[k]
+
+
+if __name__ == "__main__":
+    main()
