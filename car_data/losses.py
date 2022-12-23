54a8e4b4f494927de2de521d61a6971f780563a8
Alex Nichol <unixpickle@gmail.com>
2022-12-23 16:41:37 -0500

    loss helper

--- car_data/losses.py
+++ car_data/losses.py
@@ -1,29 +1,60 @@
-import numpy as np
+from dataclasses import dataclass
+from typing import Dict, List
 
-PRICE_CUTOFFS = [
-    10_000.0,
-    15_000.0,
-    20_000.0,
-    25_000.0,
-    30_000.0,
-    35_000.0,
-    40_000.0,
-    50_000.0,
-    60_000.0,
-]
-NUM_PRICE_BINS = len(PRICE_CUTOFFS) + 1
-PRICE_BIN_LABELS = [
-    "$0-$10,000",
-    "$10,000-$15,000",
-    "$15,000-$20,000",
-    "$20,000-$25,000",
-    "$25,000-$30,000",
-    "$30,000-$35,000",
-    "$35,000-$40,000",
-    "$40,000-$50,000",
-    "$50,000-$60,000",
-    "$60,000+",
-]
+import numpy as np
+import torch
+import torch.nn.functional as F
+
+from .constants import MAKE_MODEL_TO_INDEX, MEDIAN_PRICE_SCALE, PRICE_CUTOFFS, YEARS
+from .dataset import CarImage
+
+
+@dataclass
+class PriceTargets:
+    prices: torch.Tensor
+    price_bins: torch.Tensor
+    years: torch.Tensor
+    make_models: torch.Tensor
+
+    @classmethod
+    def from_batch(cls, batch: List[CarImage], device: torch.device) -> "PriceTargets":
+        return cls(
+            prices=torch.tensor(
+                [x.price for x in batch], dtype=torch.float32, device=device
+            ),
+            price_bins=torch.tensor([bin_price(x.price) for x in batch], device=device),
+            years=torch.tensor([bin_year(x.year) for x in batch], device=device),
+            make_models=torch.tensor(
+                [bin_make_model(x.make, x.model) for x in batch], device=device
+            ),
+        )
+
+    @classmethod
+    def from_model_out(cls, outputs: Dict[str, torch.Tensor]) -> "PriceTargets":
+        return cls(
+            prices=outputs["price_median"],
+            price_bins=F.softmax(outputs["price_bin"], dim=-1),
+            years=F.softmax(outputs["year"], dim=-1),
+            make_models=F.softmax(outputs["make_model"], dim=-1),
+        )
+
+    def metrics(self, outputs: Dict[str, torch.Tensor]) -> torch.Tensor:
+        metrics = dict(
+            price_ce=F.cross_entropy(outputs["price_bin"], self.price_bins),
+            price_acc=(
+                (outputs["price_bin"].argmax(-1) == self.price_bins).float().mean()
+            ),
+            price_mae=(outputs["price_median"] - self.prices).abs().float().mean(),
+            year_ce=F.cross_entropy(outputs["year"], self.years),
+            make_model_ce=F.cross_entropy(outputs["make_model"], self.make_models),
+        )
+        metrics["loss"] = (
+            metrics["price_ce"]
+            + (metrics["price_mae"] / MEDIAN_PRICE_SCALE)
+            + metrics["year_ce"]
+            + metrics["make_model_ce"]
+        )
+        return metrics
 
 
 def bin_price(price: float) -> int:
